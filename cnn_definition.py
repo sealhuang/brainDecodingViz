d6df54feca545ef257a40abe3f4ab8616e2aa7d8
Lijie Huang <huanglijie@outlook.com>
2016-10-17 00:23:31 +0800

    update

--- cnn_definition.py
+++ cnn_definition.py
@@ -5,8 +5,35 @@ import os
 import h5py
 import numpy as np
 import networkx as nx
-from networkx.readwrite import json_graph
 import json
+from itertools import chain
+from networkx.readwrite import json_graph
+
+def adjacency_data(G, network_info):
+    """Return data in adjacency format that is suitable for JSON serialization
+    and use in javascript documents.
+
+    Parameters
+    ----------
+    G : NetworkX graph
+    layer_names : a list with CNN layer information
+
+    Returns
+    -------
+    data : dict
+       A dictionary with adjacency formatted data.
+    
+    """
+    data = {}
+    data['network'] = network_info
+    data['weights'] = []
+    for n, nbrdict in G.adjacency_iter():
+        adj = {'connections': []}
+        for nbr, d in nbrdict.items():
+            adj['connections'].append(dict(chain([('u', nbr), ('w', d['weight'])])))
+        data['weights'].append(adj)
+    return data
+
 
 db_dir = r'/Users/sealhuang/Downloads/cnn_profile'
 cnn_file = os.path.join(db_dir, 'infor.mat')
@@ -20,24 +47,34 @@ layer_names = ['data'] + ['conv'+str(i+1) for i in range(conn_num)]
 
 # network initialization
 G = nx.DiGraph()
-
+network_info = []
+node_count = 0
 #for conn_idx in range(conn_num):
 for conn_idx in range(1):
-    src_layer = layer_names[conn_idx]
-    targ_layer = layer_names[conn_idx+1]
     wt = np.array(f.get(wts[0][conn_idx]))
     wt = wt.transpose()
     src_num, targ_num = wt.shape
     for i in range(src_num):
         for j in range(targ_num):
             if wt[i, j]:
-                G.add_edge('_'.join([src_layer, str(i+1)]),
-                           '_'.join([targ_layer, str(j+1)]),
-                           weight = wt[i, j])
+                G.add_edge(node_count+i, node_count+src_num+j,
+                           weight=abs(wt[i, j]))
+    node_count += src_num
+    # network info
+    src_layer = layer_names[conn_idx]
+    targ_layer = layer_names[conn_idx+1]
+    if not conn_idx:
+        layer_info = {'name': src_layer, 'units': src_num}
+        network_info.append(layer_info)
+    layer_info = {'name': targ_layer, 'units': targ_num}
+    network_info.append(layer_info)
 
-print json_graph.adjacency_data(G)
+#print adjacency_data(G, network_info)
+#print json_graph.adjacency_data(G)
 
-## save network into json file
-#with open('net.json', 'w') as outfile:
-#    outfile.write(json.dumps(json_graph.node_link_data(G)))
+# save network into json file
+with open('net.json', 'w') as outfile:
+    #outfile.write(json.dumps(json_graph.adjacency_data(G)))
+    outfile.write(json.dumps(adjacency_data(G, network_info)))
+
 
